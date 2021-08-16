f1c916a12d826ddc1a75b81bf40f94b967895f98
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-16 02:28:22 +0100

    ajuste get token

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:contactos/models/token_response.dart';
 import 'package:http/http.dart' as http;
 import 'package:contactos/globals/environment.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService with ChangeNotifier {
   bool _loading = false;
-
+  TokenResponse tokenResponse;
   final _storage = new FlutterSecureStorage();
   get loading => this._loading;
 
@@ -39,17 +40,17 @@ class AuthService with ChangeNotifier {
       };
 
       final response = await http.post(
-        "${Environment.apiUrl}/login",
+        "${Environment.apiUrl}/auth/access/",
         body: json.encode(data),
         headers: {
           'Content-Type': 'application/json',
         },
       );
       if (response.statusCode == 200) {
-        final loginResponse = loginResponseFromJson(response.body);
-        this.user = loginResponse.user;
+        final loginResponse = tokenResponseFromJson(response.body);
+        this.tokenResponse = loginResponse;
 
-        await this._saveToken(loginResponse.token);
+        await this._saveToken(loginResponse.access);
         return true;
       } else {
         return false;
@@ -60,64 +61,66 @@ class AuthService with ChangeNotifier {
   }
 
   Future _saveToken(String token) async {
-    return await _storage.write(key: "token", value: token);
+    return await _storage.write(key: "access", value: token);
   }
 
-  Future register(String name, String email, String password) async {
-    try {
-      loading = true;
-      final data = {
-        "name": name,
-        "email": email,
-        "password": password,
-      };
+  // Future register(
+  //   String user,
+  //   String pass,
+  // ) async {
+  //   try {
+  //     loading = true;
+  //     final data = {
+  //       "user": user,
+  //       "pass": pass,
+  //     };
 
-      final response = await http.post(
-        "${Environment.apiUrl}/login/new",
-        body: json.encode(data),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      );
-      if (response.statusCode == 200) {
-        final registerResponse = registerResponseFromJson(response.body);
-        this.user = registerResponse.user;
+  //     final response = await http.post(
+  //       "${Environment.apiUrl}/auth/access/",
+  //       body: json.encode(data),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     );
+  //     if (response.statusCode == 200) {
+  //       final registerResponse = registerResponseFromJson(response.body);
+  //       this.user = registerResponse.user;
 
-        await this._saveToken(registerResponse.token);
-        return true;
-      } else {
-        final respBody = jsonDecode(response.body);
-        print(respBody);
-        return respBody["msg"];
-      }
-    } catch (e) {} finally {
-      loading = false;
-    }
-  }
+  //       await this._saveToken(registerResponse.token);
+  //       return true;
+  //     } else {
+  //       final respBody = jsonDecode(response.body);
+  //       print(respBody);
+  //       return respBody["msg"];
+  //     }
+  //   } catch (e) {} finally {
+  //     loading = false;
+  //   }
+  // }
 
-  Future<bool> isLoggedIn() async {
-    final token = await this._storage.read(key: "token");
-    final response =
-        await http.get('${Environment.apiUrl}/login/renew', headers: {
-      'Content-Type': 'application/json',
-      'x-token': token,
-    });
+  // Future<bool> isLoggedIn() async {
+  //   final token = await this._storage.read(key: "token");
+  //   final response =
+  //       await http.get('${Environment.apiUrl}/login/renew', headers: {
+  //     'Content-Type': 'application/json',
+  //     'x-token': token,
+  //   });
 
-    print(response.body);
+  //   print(response.body);
 
-    if (response.statusCode == 200) {
-      final loginResponse = loginResponseFromJson(response.body);
-      this.user = loginResponse.user;
+  //   if (response.statusCode == 200) {
+  //     final loginResponse = loginResponseFromJson(response.body);
+  //     this.user = loginResponse.user;
 
-      await this._saveToken(loginResponse.token);
-      return true;
-    } else {
-      this._logout();
-      return false;
-    }
-  }
+  //     await this._saveToken(loginResponse.token);
+  //     return true;
+  //   } else {
+  //     this._logout();
+  //     return false;
+  //   }
+  // }
 
   Future _logout() async {
-    return await _storage.delete(key: "token");
+    return await _storage.delete(key: "access");
   }
 }
