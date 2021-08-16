f7612f12722b737cdf26039845491289c0a2b03c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-16 04:22:50 +0100

    get contacts

--- lib/screens/contacts_screen.dart
+++ lib/screens/contacts_screen.dart
@@ -1,4 +1,7 @@
+import 'package:contactos/models/contacts.dart';
+import 'package:contactos/services/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ContactsScreen extends StatefulWidget {
   const ContactsScreen({Key key}) : super(key: key);
@@ -11,178 +14,182 @@ class _ContactsScreenState extends State<ContactsScreen> {
   final RefreshController _refreshController =
       RefreshController(initialRefresh: false);
 
-  final userService = UsersService();
+  final userService = ContactsService();
 
-  List<User> users = [];
+  List<Result> contacts = [];
 
   @override
   void initState() {
     super.initState();
-    this._cargarusers();
+    this._getContacts();
   }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Contactos",
-                          style: TextStyle(color: Colors.white, fontSize: 20)),
-                      Icon(
-                        Icons.add_box_rounded,
-                        size: 40,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 14,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey[800],
-                        borderRadius: BorderRadius.circular(30)),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        focusColor: Colors.transparent,
-                        hintText: 'Procurar',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              const BorderSide(color: Colors.white, width: 2.0),
-                        ),
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        contentPadding: const EdgeInsets.only(
-                            top: 15, left: 10, right: 10, bottom: 15),
-                        errorMaxLines: 1,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "Recentes",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Container(
-                    height: 40,
-                    width: double.infinity,
-                    child: ListView(
-                      physics: BouncingScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
+        child: Container(
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
-                          ),
-                        ),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
-                          ),
-                        ),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
-                          ),
-                        ),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
-                          ),
-                        ),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
-                          ),
-                        ),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          margin: EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
-                          ),
+                        Text("Contactos",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                        Icon(
+                          Icons.add_box_rounded,
+                          size: 40,
+                          color: Colors.white,
                         ),
                       ],
                     ),
-                  )
-                ],
-              ),
-            ),
-            Spacer(),
-            Container(
-              height: (size.height * 0.7),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[800],
+                          borderRadius: BorderRadius.circular(30)),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          focusColor: Colors.transparent,
+                          hintText: 'Procurar',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          prefixIcon: Icon(Icons.search, color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 2.0),
+                          ),
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          contentPadding: const EdgeInsets.only(
+                              top: 15, left: 10, right: 10, bottom: 15),
+                          errorMaxLines: 1,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "Recentes",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Container(
+                      height: 40,
+                      width: double.infinity,
+                      child: ListView(
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/1407315372819324928/Vhst6oDe_400x400.jpg")),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-          ],
+              Spacer(),
+              Container(
+                height: (size.height * 0.7),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void _getContacts()async{
-      this.users = await userService.getUsers();
+  void _getContacts() async {
+    this.contacts = await userService.getContacts();
 
     setState(() {});
     _refreshController.refreshCompleted();
--- lib/services/contacts_service.dart
+++ lib/services/contacts_service.dart
@@ -9,14 +9,13 @@ import 'package:http/http.dart' as http;
 import 'auth_service.dart';
 
 class ContactsService with ChangeNotifier {
- 
   Future<List<Result>> getContacts() async {
     try {
-      final response = await http.get("${Environment.apiUrl}/contacts",
-          headers: {
-            "Content-type": "application/json",
-            'x-token': await AuthService.getToken()
-          });
+      final response =
+          await http.get("${Environment.apiUrl}/contacts", headers: {
+        "Content-type": "application/json",
+        'Authorization bearer': await AuthService.getToken(),
+      });
 
       final usersResponse = contactsResponseFromJson(response.body);
       print(usersResponse.results);
